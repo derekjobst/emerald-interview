607dd8980ed9bf97158d8e2dc0ff03e136c6b01f
Derek Jobst <derekjobst@gmail.com>
2014-03-10 16:09:10 -0700

    comments

--- acronym/acronym.coffee
+++ acronym/acronym.coffee
@@ -1,6 +1,6 @@
 
 # Used http://coffeescript.org for this.
-
+# I've only played around with CoffeeScript before, so this may not be entirely in the correct syntax.
 
 acronym = (input) ->
 	out = ""
--- acronym/acronym.js
+++ acronym/acronym.js
@@ -4,10 +4,12 @@ function acronym(input){
 
 	var out = "";
 
+	//check if first letter is not a space b/c while loop will hit first word only if it is preceeded by a space.
 	if(input.charAt(0) != " "){
 		out = out.concat(input.charAt(0));
 	}
 
+	//iterate through all spaces, concat letter succeeding them IFF not a space.
 	while(input.indexOf(" ") != -1){
 
 		var location = input.indexOf(" ") + 1;
