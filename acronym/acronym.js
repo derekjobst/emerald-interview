a34927745056dd14dd496995ec65be326eb06a8f
Derek Jobst <derekjobst@gmail.com>
2014-03-10 15:26:06 -0700

    finished JS

---
+++ acronym/acronym.js
@@ -0,0 +1,23 @@
+/* Derek Jobst 3/10/14 */
+
+function acronym(input){
+
+	var out = "";
+
+	if(input.charAt(0) != " "){
+		out = out.concat(input.charAt(0));
+	}
+
+	while(input.indexOf(" ") != -1){
+
+		var location = input.indexOf(" ") + 1;
+
+		if(input.charAt(location) != " "){
+			out = out.concat(input.charAt(0));
+		}
+
+		input = input.substr(location+1);
+	}
+
+	return out;
+}
